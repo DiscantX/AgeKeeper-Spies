e308d3427833698f5ec69ccd95fd1772d1284c4d
DiscantX <DiscantX@hotmail.com>
2026-02-10 18:54:29 -0700

    Major: lobby.py, spies.py, project structure

---
+++ player_test.py
@@ -0,0 +1,13 @@
+import lobby
+
+profileid = 10056062
+
+def player(event):
+    print(event)
+
+def main():
+    subscriptions = lobby.subscribe(["players"], player_ids = [profileid])
+    lobby.connect_to_subscriptions(subscriptions, player)
+    
+if __name__ == "__main__":
+    main()
---
+++ spies.py
@@ -0,0 +1,113 @@
+from windows_toasts import(
+        InteractableWindowsToaster, Toast,
+        ToastDisplayImage, ToastImagePosition,
+        ToastActivatedEventArgs, ToastDismissedEventArgs, ToastFailedEventArgs
+    )
+from lobby import lobby
+import urllib.request
+import time
+from webbrowser import open_new as web_open
+from pathlib import Path
+from functools import partial
+
+temp_file_path = "spies/temp_files/temp_image.png" 
+default_avatar_path = "spies/assets/default_avatar.png"
+toaster = InteractableWindowsToaster('AOE2: Spies')
+spyToast = Toast('Spy Alert')
+
+player_name = "Necron99"
+profileid = 10056062
+
+def get_player_avatar(player_name: str, match):
+    player_slot = lobby.get_player_slot(player_name, match)
+    avatar_url = player_slot.get("steam_avatar", None)
+    avatar_url = None
+    if avatar_url:
+        avatar_filepath = download_image(avatar_url)
+    else:
+        print("No avatar URL found, using default avatar.")
+        avatar_filepath = default_avatar_path
+    return avatar_filepath
+
+def add_player_avatar_to_toast(player_name: str, match):
+    filepath = get_player_avatar(player_name, match)
+    spyToast.AddImage(ToastDisplayImage.fromPath(filepath, position = ToastImagePosition.AppLogo))
+
+# Helper function to download an image and return local path
+def download_image(url, filepath=temp_file_path):
+    path, _ = urllib.request.urlretrieve(url, filepath)
+    return str(Path(path).resolve())
+
+def remove_image(filepathh=temp_file_path):
+    try:
+        Path(filepathh).unlink()
+        print("Avatar image removed.")
+    except FileNotFoundError:
+        pass
+
+def on_toast_dismissed():
+    print("Toast dismissed.")
+
+def activated_callback(activatedEventArgs: ToastActivatedEventArgs, short_response_type: str, match_id: str):
+    print(f"Toast activated for {short_response_type} with Match ID: {match_id}")
+    if short_response_type == "Lobby":
+        response_type_id = 0
+    elif short_response_type == "Spectate":
+        response_type_id = 1
+    else:
+        print("Unknown response type, cannot open game.")
+        return
+    response = web_open(f"aoe2de://{response_type_id}/{match_id}")
+    print(f"Web open response: {response}")
+    remove_image()
+
+def dismissed_callback(dismissedEventArgs: ToastDismissedEventArgs):
+    print(f"Toast dismissed reason: {dismissedEventArgs.reason}")
+    remove_image()
+
+def failed_callback(failedEventArgs: ToastFailedEventArgs):
+    print(f"Toast failed: {failedEventArgs.reason}")
+
+def display_toast(player_name: str, match, event):  
+    short_response_type = lobby.get_short_response_type(event)
+    player_data = lobby.get_player_slot(player_name, match)
+    player_civ_name = lobby.get_civ_name(player_data.get("civilization", -1))
+    
+    time_dilation = 7 # Server time is approx 7 seconds ahead of local time, so we subtract 7 seconds from the match creation time to get a more accurate "time alive" for the match
+    created_time = match.get("created_time", int(time.time())) - time_dilation
+    match_time_alive = int(time.time()) - created_time
+    
+    print(f"{player_name} id is {player_data.get('profileid', 'Unknown')}")
+        
+    toast_fields = [
+        f"{player_name[:25]} joined {short_response_type.lower()}:\n{match.get('description', 'a {short_response_type.lower()}')}",
+        f"Map: {match.get('map_name', 'Unknown Map')} | Playing as: {player_civ_name}",
+        f"Started: {match_time_alive}s ago | {match.get('slots_taken', -1)} Player{"s" if match.get("slots_taken", -1) != 1 else ""} in {short_response_type.lower()}"
+        ]
+     
+    spyToast.on_activated = partial(activated_callback, short_response_type=short_response_type, match_id=match.get("matchid", -1))
+    spyToast.on_dismissed = dismissed_callback
+    spyToast.on_failed = failed_callback
+    spyToast.text_fields = toast_fields
+    spyToast.AddImage(ToastDisplayImage.fromPath('assets/AgeKeeperBanner_Cropped.png', position = ToastImagePosition.Hero))
+    add_player_avatar_to_toast(player_name, match)
+    
+    toaster.show_toast(spyToast)
+    
+    print("\nNew Spy Alert:")
+    print("="*40)
+    print("\n".join(toast_fields))
+
+def spy(event, **kwargs):
+    matches = lobby.get_new_match_ids(event)
+    results = lobby.search_matches_for_player(event, player_name=player_name, match_ids=matches)
+    match = lobby.get_match_by_id(event, results[0]) if results else None
+    if results:
+        display_toast(player_name=player_name, match=match, event=event)
+
+def main():
+    subscriptions = lobby.subscribe(["lobby", "spectate", "players"], player_ids = [profileid])
+    lobby.connect_to_subscriptions(subscriptions, spy)
+
+if __name__ == "__main__":
+    main()
